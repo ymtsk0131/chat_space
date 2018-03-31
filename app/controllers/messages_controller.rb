3b5c9df707be4596aa8e774fcb92dd7af67b3cd0
motosaka <ymtsk0131@live.jp>
2018-03-31 16:40:56 +0900

    メッセージ送信機能の実装（画像未実装）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user).order("created_at DESC")
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    @message.save
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group =Group.find(params[:group_id])
+  end
 end
