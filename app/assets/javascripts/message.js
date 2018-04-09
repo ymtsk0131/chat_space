c895375e0640ff850435eaf1fdc2920481f9ae0b
motosaka <ymtsk0131@live.jp>
2018-04-09 11:42:04 +0900

    画像送信の非同期化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,6 +6,14 @@ $(function(){
                 <div class="right-content-message__message-text">${message.content}</div>`
     return html;
   }
+
+  function imageHTML(message){
+    var html = `<div class="right-content-message__user-name">${message.user_name}</div>
+                <div class="right-content-message__date">${message.created_at}</div>
+                <div class="right-content-message__message-text"><img src="${message.image_url}"></div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -19,9 +27,11 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data)
-      var html = messageHTML(data);
-      console.log(html)
+      if(data.content){
+        var html = messageHTML(data);
+      }else if(data.image_url){
+        var html = imageHTML(data);
+      }
       $('.right-content-message').append(html)
       $('html, body').animate({
           scrollTop: $(document).height()
