d314e253ece80dc5e2c44865888f2ee273e1aed4
motosaka <ymtsk0131@live.jp>
2018-04-10 23:56:29 +0900

    HTMLの生成

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,5 +1,17 @@
 $(function(){
+
+var users = $('#chat-group-users')
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`
+  users.append(html)
+}
+
   $(document).on('keyup', '#user-search-field', function(){
+    $('#chat-group-users').empty();
     var input = $('#user-search-field').val();
     $.ajax({
       type: 'GET',
@@ -9,8 +21,10 @@ $(function(){
     })
 
     .done(function(users) {
-      console.log(users)
-    });
+      users.forEach(function(user){
+        appendUser(user)
+      });
+    })
   });
 });
 
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array! @users do |user|
-  json.user_name  user.name
-  json.user_id    user.id
+  json.name  user.name
+  json.id    user.id
 end
