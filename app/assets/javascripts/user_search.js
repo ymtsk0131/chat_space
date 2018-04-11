3887b3a6e96255a19524805756112fce16e70d31
motosaka <ymtsk0131@live.jp>
2018-04-11 20:39:19 +0900

    追加削除のイベント発生とHTML生成

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,6 +1,7 @@
 $(function(){
 
 var users = $('#chat-group-users')
+var members = $('#chat-group-members')
 
 function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
@@ -8,6 +9,15 @@ function appendUser(user) {
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
               </div>`
   users.append(html)
+}
+
+function addUser() {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+                <p class='chat-group-user__name'>ユーザー名</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+  members.append(html)
 }
 
   $(document).on('keyup', '#user-search-field', function(){
@@ -30,5 +40,16 @@ function appendUser(user) {
       alert('ユーザーの検索に失敗しました')
     })
   });
+
+  $(document).on('click', '#chat-group-users > div:nth-child(1) > a', function(){
+    addUser();
+    $('#chat-group-users > div:nth-child(1)').remove();
+    console.log("add");
+  })
+
+  $(document).on('click', '#chat-group-user-8 > a', function(){
+    $('#chat-group-user-8').remove();
+    console.log("remove")
+  })
 });
 
