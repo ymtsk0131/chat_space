d5474acd32a387e49006661dd286bbae774795a7
motosaka <ymtsk0131@live.jp>
2018-04-11 21:55:02 +0900

    追加後候補から消えるように変更

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -4,7 +4,7 @@ var users = $('#chat-group-users')
 var members = $('#chat-group-members')
 
 function appendUser(user) {
-  var html = `<div class="chat-group-user clearfix">
+  var html = `<div class="chat-group-user clearfix" id="chat-group-user-${user.id}">
                 <p class="chat-group-user__name">${user.name}</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
               </div>`
@@ -41,9 +41,11 @@ function addUser() {
     })
   });
 
-  $(document).on('click', '#chat-group-users > div:nth-child(1) > a', function(){
+  $(document).on('click', '#chat-group-users', function(){
+    var user_id = $('.chat-group-user__btn--add').attr('data-user-id')
+    console.log(user_id)
     addUser();
-    $('#chat-group-users > div:nth-child(1)').remove();
+    $('#chat-group-user-' + user_id).remove();
     console.log("add");
   })
 
