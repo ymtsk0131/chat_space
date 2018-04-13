97dad01fad76a389e0726e6d24bb5d1fc370c3bf
motosaka <ymtsk0131@live.jp>
2018-04-13 15:00:12 +0900

    不要な記述の削除＆シンボル型に統一

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-  resources :users, :only => [:index]
+  resources :users, only: [:index]
 end
