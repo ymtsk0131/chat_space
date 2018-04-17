d9bd8acba0ab934e4071e408cf4eb672f02a94a9
motosaka <ymtsk0131@live.jp>
2018-04-17 22:07:09 +0900

    グループメッセージのページでのみ非同期通信を行う

--- app/assets/javascripts/message_update.js
+++ app/assets/javascripts/message_update.js
@@ -15,29 +15,28 @@ $(function(){
   setInterval(function(){
     var latest_message = $('.right-content-message__user-name:last')
     var message_id = latest_message.length ? latest_message.data("id") : 0
-
-    $.ajax({
-      url: location.href,
-      type: 'GET',
-      data: { id: message_id },
-      dataType: 'json'
-    })
-
-    .done(function(data){
-      if (data.length){
-        data.forEach(function(new_message){
-          var img = new_message.image_url ? imageHTML(new_message) : '';
-          var html = messageHTML(new_message)
-          $('.right-content-message').append(html).append(img)
-        })
-        $('html, body').animate({
-            scrollTop: $(document).height()
-          },1500);
-      }
-    })
-
-    .fail(function(){
-      alert("自動更新が失敗しました")
-    })
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: { id: message_id },
+        dataType: 'json'
+      })
+      .done(function(data){
+        if (data.length){
+          data.forEach(function(new_message){
+            var img = new_message.image_url ? imageHTML(new_message) : '';
+            var html = messageHTML(new_message)
+            $('.right-content-message').append(html).append(img)
+          })
+          $('html, body').animate({
+              scrollTop: $(document).height()
+            },1500);
+        }
+      })
+      .fail(function(){
+        alert("自動更新に失敗しました")
+      })
+    }
   },5000);
 });
