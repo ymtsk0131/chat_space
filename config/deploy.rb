ef46589b2a490fcd615e6d23956a7db2e3e24fdc
motosaka <ymtsk0131@live.jp>
2018-04-24 16:09:31 +0900

    S3導入

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,26 @@ set :default_env, {
   :DEVISE_SECRET_KEY => ENV['DEVISE_SECRET_KEY']
 }
 
+set :linked_files, %w{ config/secrets.yml }
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # Default branch is :master
