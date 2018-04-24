ad88ee941d7f96141cfd1812c56a0dc8e052d2d2
motosaka <ymtsk0131@live.jp>
2018-04-24 11:28:19 +0900

    capostrano導入、記述ミスの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'http://13.114.153.58/', user: 'ec2-user', roles: %w{app db web}
+server '13.114.153.58', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
