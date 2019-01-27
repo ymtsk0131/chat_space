83226afe3067b9d6a54aa5be44736a964dc544b1
ymtsk0131 <ymtsk0131@live.jp>
2019-01-27 12:24:54 +0900

    Patch install React

--- react/src/groups/index/index.js
+++ react/src/groups/index/index.js
@@ -1,1 +1,5 @@
-console.log('Hello world')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('group-index'));
