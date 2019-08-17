1357626018438dd420b1a4f91162a420ce3e2821
yuki mtsk <36557078+ymtsk0131@users.noreply.github.com>
2019-08-17 11:46:07 +0900

    Feature/Install Vue JS (#23)
    * Install vuejs axios
    
    * Add rubocop todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+  root to: 'home#index'
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
