ca78c71a4202368eb6e0f488b10fa78ab405bed5
yuki mtsk <yuki.mtsk0131+github@gmail.com>
2019-09-14 17:52:21 +0900

    Modify style

--- app/javascript/components/modules/SideMenu.vue
+++ app/javascript/components/modules/SideMenu.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="sidebar" class="p-2">
-    <h3>Chat Space</h3>
-    <h5>チャンネル</h5>
-    <ul class="group_list">
-      <router-link v-for="g in groups" :key="g.id" :to="{ name: 'GroupMessages', params: { id: g.id } }">
-        <li># {{ g.name }}</li>
-      </router-link>
-    </ul>
+  <div id="side_menu">
+    <div class="hover_item p-2 mb-3">
+      <div id="sidemenu_title" class="font-weight-bold text-white">Chat Space</div>
+      <div>test taro</div>
+    </div>
+    <div class="mb-3">
+      <div class="font-weight-bold pl-2">チャンネル</div>
+      <ul class>
+        <router-link v-for="g in groups" :key="g.id" :to="{ name: 'GroupMessages', params: { id: g.id } }" class="text-reset">
+          <li class="hover_item pl-2 py-1"># {{ g.name }}</li>
+        </router-link>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -28,13 +33,22 @@ export default {
 </script>
 
 <style scoped>
-  #sidebar {
+  #side_menu {
     background-color: #3E0E40;
-    color: #fff!important;
+    color: rgb(207,195,207);
     height: 100vh;
     width: 250px;
     position: fixed;
     top: 0;
     left: 0;
+  }
+
+  #sidemenu_title {
+    font-size: 18px;
+  }
+
+  .hover_item:hover {
+    background-color: #340b36;
+    color: #fff;
   }
 </style>
